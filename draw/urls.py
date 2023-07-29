92648c16826998f23b3ae4bf3327caba11ef94ca
m.alaii <alaii.hit@gmail.com>
2023-07-29 19:22:53 +0330

    add temporary api endpont for drawing

--- draw/urls.py
+++ draw/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', draw_view)
+    path('', draw_view),
+    path('4631f492-e29b-4b75-92f0-68de73580db8', draw_chart)
 ]
--- draw/views.py
+++ draw/views.py
@@ -1,6 +1,16 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 
 def draw_view(request):
     return render(request, 'draw.html')
+
+
+def draw_chart(request):
+    # request.POST['diagram_name']
+    # request.POST['dataset']
+    # request.POST['target']
+    # json.loads(request.POST['variables'])
+    return HttpResponse(status=200)
