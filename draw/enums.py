7cc27a46a7c2a648594e9e3278ad5d74ecb981d9
sina <hariri.sina.s@gmail.com>
2023-08-04 14:49:55 +0330

    fixed the problems for all the charts regarding target and variables in arguments

--- draw/enums.py
+++ draw/enums.py
@@ -5,7 +5,7 @@ class Chart(Enum):
     LINE_CHART = "Line Chart"
     STACKED_LINE_CHART = "Stacked Line Chart"
     AREA_CHART = "Area Chart"
-    STACKED_AREA_CHART = "Stacked Line Chart"
+    STACKED_AREA_CHART = "Stacked Area Chart"
     DOT_LINE_CHART = "Dot Line Chart"
     HEAT_MAP_CHART = "Heat Map Chart"
     DENSITY_CHART = "Density Chart"
--- draw/pen.py
+++ draw/pen.py
@@ -13,7 +13,7 @@ def pen(diagram_name, dataset, target, variables) -> str:
     if diagram_name == Chart.STACKED_LINE_CHART.value:
         return plot_stacked_line_chart(dataset, target, variables)
 
-    if diagram_name == Chart.AREA_CHART or diagram_name == Chart.STACKED_AREA_CHART:
+    if diagram_name == Chart.AREA_CHART.value or diagram_name == Chart.STACKED_AREA_CHART.value:
         return plot_stacked_area_chart(dataset, target, variables)
 
     if diagram_name == Chart.DOT_LINE_CHART.value:
@@ -27,16 +27,16 @@ def pen(diagram_name, dataset, target, variables) -> str:
 
     if diagram_name == Chart.BUBBLE_CHART.value:
         return plot_bubble_chart(dataset, target, variables)
-    
+
     if diagram_name == Chart.SCATTER_CHART.value:
         return plot_scatter_chart(dataset, target, variables)
-    
+
     if diagram_name == Chart.SCATTER_MATRIX_CHART.value:
         return plot_scatter_matrix_chart(dataset, target, variables)
-    
+
     if diagram_name == Chart.PARALLEL_COORDINATES_CHART.value:
         return plot_parallel_coordinates_chart(dataset, target, variables)
-    
+
     if diagram_name == Chart.BAR_CHART.value:
         return plot_bar_chart(dataset, target, variables)
 
@@ -49,6 +49,7 @@ def pen(diagram_name, dataset, target, variables) -> str:
     if diagram_name == Chart.TREEMAP_CHART.value:
         return plot_treemap_chart(dataset, target, variables)
 
+
 def plot_line_chart(dataset, target, variables) -> str:
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
@@ -58,103 +59,48 @@ def plot_line_chart(dataset, target, variables) -> str:
     return file_name
 
 
-def plot_stacked_line_chart(dataset, target, variables):
+def plot_stacked_line_chart(dataset, target, variables) -> str:
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
-    fig = px.line(data_frame=df, x=variables[0], y=variables[1], color=target)
+    fig = px.line(data_frame=df, x=variables[0], y=target, color=variables[1])
     file_name = f"{uuid.uuid4()}.html"
     fig.write_html(f"media/{file_name}")
     return file_name
 
 
-def plot_stacked_area_chart(dataset, target, variables):
+def plot_stacked_area_chart(dataset, target, variables) -> str:
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
 
-    # Create the traces for the stacked line chart
-    traces = []
-    for variable in variables:
-        trace = go.Scatter(
-            x=df[target],
-            y=df[variable],
-            mode='lines',
-            stackgroup='one',
-            name=""
-        )
-        traces.append(trace)
-
-    # Create the layout for the chart
-    layout = go.Layout(
-        title='Stacked Line Chart',
-        xaxis=dict(title=target),
-        yaxis=dict(title='Value'),
-        hovermode='closest'
-    )
-
-    # Create the figure with the traces and layout
-    fig = go.Figure(data=traces, layout=layout)
+    fig = px.area(df, x=variables[0], y=target, color=variables[1])
 
     file_name = f"{uuid.uuid4()}.html"
     fig.write_html(f"media/{file_name}")
     return file_name
 
 
-def plot_dot_line_chart(dataset, target, variables):
+def plot_dot_line_chart(dataset, target, variables) -> str:
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
+    # df.sort_values(by=variables +[target], inplace=True)
 
-    # Get x-axis label from the chosen column
-    x_column = target
-
-    # Get y-axis labels from the chosen columns
-    y_columns = variables
-
-    # Sort the values in the chosen x-axis and y-axis columns
-    df.sort_values(by=[x_column] + y_columns, inplace=True)
-
-    # Create the traces for the dot and line chart
-    traces = []
-    for y_column in y_columns:
-        trace = go.Scatter(
-            x=df[x_column],
-            y=df[y_column],
-            mode='lines+markers',
-            name=y_column
-        )
-        traces.append(trace)
-
-    # Create the layout for the chart
-    layout = go.Layout(
-        title='Dot and Line Chart',
-        yaxis=dict(title='Value'),
-        hovermode='closest'
-    )
-
-    # Create the figure with the traces and layout
-    fig = go.Figure(data=traces, layout=layout)
-
+    fig = px.line(df, x=variables[0], y=target, color=variables[1] if len(variables) > 1 else None, markers=True)
     file_name = f"{uuid.uuid4()}.html"
     fig.write_html(f"media/{file_name}")
     return file_name
 
 
-def plot_heat_map_chart(dataset, target, variables):
+def plot_heat_map_chart(dataset, target, variables) -> str:
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
-
-    # Get x-axis label from the chosen column
-    x_column = target
-
-    # Get y-axis label from the chosen column
-    y_column = variables[0]
 
     # Get values (z-axis) from the chosen column
     z_values = df[variables[1]].values
 
     # Create the trace for the heatmap
     trace = go.Heatmap(
-        x=df[x_column],
-        y=df[y_column],
+        x=df[variables[0]],
+        y=df[target],
         z=z_values,
         colorscale='Viridis',
         zmin=df[variables[1]].min(),
@@ -175,7 +121,7 @@ def plot_heat_map_chart(dataset, target, variables):
     return file_name
 
 
-def plot_density_chart(dataset, target, variables):
+def plot_density_chart(dataset, target, variables) -> str:
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
 
@@ -215,7 +161,7 @@ def plot_density_chart(dataset, target, variables):
     return file_name
 
 
-def plot_bubble_chart(dataset, target, variables):
+def plot_bubble_chart(dataset, target, variables) -> str:
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
 
@@ -259,9 +205,9 @@ def plot_scatter_chart(dataset, target, variables):
     fig = px.scatter(
         data_frame=df,
         x=variables[0],
-        y=variables[1],
-        color=target,
-        symbol=target
+        y=target,
+        color=variables[1],
+        symbol=variables[1]
     )
     file_name = f"{uuid.uuid4()}.html"
     fig.write_html(f"media/{file_name}")
@@ -273,8 +219,8 @@ def plot_scatter_matrix_chart(dataset, target, variables):
     df = pd.read_csv(data, sep=",")
 
     fig = px.scatter_matrix(
-        data_frame=df, 
-        dimensions=variables if len(variables) > 1 else None, 
+        data_frame=df,
+        dimensions=variables if len(variables) > 1 else None,
         color=target if target != "" else None
     )
 
@@ -287,7 +233,7 @@ def plot_parallel_coordinates_chart(dataset, target, variables):
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
     fig = px.parallel_coordinates(
-        data_frame=df, 
+        data_frame=df,
         color=target,
         dimensions=variables,
         color_continuous_scale=px.colors.diverging.Tealrose,
@@ -303,8 +249,8 @@ def plot_bar_chart(dataset, target, variables):
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
     fig = px.bar(
-        df, 
-        x=variables[0], 
+        df,
+        x=variables[0],
         y=target
     )
 
@@ -317,8 +263,8 @@ def plot_stacked_bar_chart(dataset, target, variables):
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
     fig = px.bar(
-        df, 
-        x=variables[0], 
+        df,
+        x=variables[0],
         y=target,
         color=variables[1]
     )
@@ -352,6 +298,7 @@ def plot_table_chart(dataset, target, variables):
     fig.write_html(f"media/{file_name}")
     return file_name
 
+
 def plot_treemap_chart(dataset, target, variables):
     data = StringIO(dataset)
     df = pd.read_csv(data, sep=",")
@@ -360,13 +307,12 @@ def plot_treemap_chart(dataset, target, variables):
     values = variables.pop()
 
     fig = px.treemap(
-        df, 
-        path=variables, 
+        df,
+        path=variables,
         values=values,
         color=target
     )
-    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-
+    fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
 
     file_name = f"{uuid.uuid4()}.html"
     fig.write_html(f"media/{file_name}")
