3a4931d6e9230922af05ecf6818b222fe3928069
m.alaii <alaii.hit@gmail.com>
2023-08-19 22:56:37 +0330

    connect front and backend to fetch questions

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 
 def next_node(request):
-    next_node_id = request.POST['next_node_id']
+    next_node_id = json.loads(request.body)['next_node_id']
     
     with open(os.path.join(settings.BASE_DIR, "core", "utils", "model.json"), "r", encoding="utf-8") as f:
         model_json = json.loads(f.read())
---
+++ recommend/static/js/recommend.js
@@ -0,0 +1,126 @@
+const question = document.querySelector("#card_question p")
+const answer_yes = document.querySelector("#card_answer_yes div")
+const answer_no = document.querySelector("#card_answer_no div")
+const question_hint = document.querySelector("#card_question_hint")
+const yes_hint = document.querySelector("#card_yes_hint")
+const no_hint = document.querySelector("#card_no_hint")
+const dialog_wrapper = document.querySelector(".dialog_wrapper")
+const dialog_title = document.querySelector(".dialog_title")
+const dialog_content = document.querySelector(".dialog_content")
+const dialog_btn_yes = document.querySelector(".dialog_yes_btn")
+const dialog_btn_no = document.querySelector(".dialog_no_btn")
+const container = document.querySelector(".container")
+
+
+
+
+async function get_next_question(next_node_id) {
+    response = await fetch("http://127.0.0.1:8000/core/ee85d908-f8f2-473e-a666-8c4d12507f5e", {
+        method: 'POST',
+        body: JSON.stringify({next_node_id: next_node_id})
+    })
+
+    return await response.json()
+}
+
+function load_node_data(data) {
+    question.innerHTML = data['phrase']
+    answer_yes.setAttribute('data-nextid', data['yes_node'])
+    answer_no.setAttribute('data-nextid', data['no_node'])
+
+    if (data['phrase_hint'] != "") {
+        question_hint.style.display = "flex"
+        question_hint.setAttribute("data-hint", data['phrase_hint'])
+    } else {
+        question_hint.style.display = "none"
+    }
+
+    if (data['yes_hint'] != "") {
+        yes_hint.style.display = "flex"
+        yes_hint.setAttribute("data-hint", data['yes_hint'])
+    } else {
+        yes_hint.style.display = "none"
+    }
+
+    if (data['no_hint'] != "") {
+        no_hint.style.display = "flex"
+        no_hint.setAttribute("data-hint", data['no_hint'])
+    } else {
+        no_hint.style.display = "none"
+    }
+
+    if (data['no_url'] != "") {
+        yes_hint.setAttribute("data-hint-url", data['yes_url'])
+    }
+
+    if (data['yes_url'] != "") {
+        no_hint.setAttribute("data-hint-url", data['no_url'])
+    }
+
+}
+
+async function answer_clicked(elmnt) {
+    answer_data = elmnt.getAttribute('data-nextid')
+    if (parseInt(answer_data)) {
+        load_node_data(await get_next_question(answer_data))
+    } else {
+
+    }
+}
+
+const dialog = (show, title = "", content = "") => {
+    let dialog_display = show ? "flex" : "none"
+
+    dialog_wrapper.style.display = dialog_display
+    dialog_title.innerText = title
+    dialog_content.innerText = content
+
+    if (show) {
+        container.classList.add("blured")
+    } else {
+        container.classList.remove("blured")
+    }
+}
+
+dialog_btn_yes.onclick = () => {
+    dialog(false)
+}
+
+dialog_wrapper.onclick = (e) => {
+    if (e.target == dialog_wrapper) {
+        dialog(false)
+    }
+}
+
+question_hint.onclick = (e) => {
+    dialog(true, "راهنما", question_hint.getAttribute("data-hint"))
+    dialog_btn_no.style.display = "none"
+}
+
+yes_hint.onclick = (e) => {
+    if (e.target == yes_hint || e.target == yes_hint.querySelector("i") || e.target == yes_hint.querySelector("span")) {
+        dialog(true, "راهنما", yes_hint.getAttribute("data-hint"))
+        if (yes_hint.getAttribute("data-hint-url") && yes_hint.getAttribute("data-hint-url") != "") {
+            dialog_btn_no.style.display = "flex"
+            dialog_btn_no.getAttribute("data-hint-url")
+        } else {
+            dialog_btn_no.style.display = "none"
+        }
+    }
+}
+
+no_hint.onclick = (e) => {
+    if (e.target == no_hint) {
+        dialog(true, "راهنما", no_hint.getAttribute("data-hint"))
+        if (no_hint.getAttribute("data-hint-url") && no_hint.getAttribute("data-hint-url") != "") {
+            dialog_btn_no.style.display = "flex"
+            dialog_btn_no.getAttribute("data-hint-url")
+        } else {
+            dialog_btn_no.style.display = "none"
+        }
+    }
+}
+
+(async () => {
+    load_node_data(await get_next_question("0"))
+})()
